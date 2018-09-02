c247c916e5ca681e39102e4691d98b3d00a645ce
Richard Tzanov <richardtzanov@gmail.com>
2018-09-02 13:06:23 +0100

    created mutation type and resolver

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -6,33 +6,28 @@ const links = [{
   description: 'fallback tutorial for GraphQL'
 }];
 
-const typeDefs = `
-type Query {
-  info: String!
-  feed: [Link!]!
-}
-
-type Link {
-  id: ID!
-  description: String!
-  url: String!
-}
-`;
+let idCount = links.length;
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
     feed: () => links
   },
-  Link: {
-    id: (root) => root.id,
-    description: (root) => root.description,
-    url: (root) => root.url
+  Mutation: {
+    post: (root, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url
+      };
+      links.push(link);
+      return link;
+    }
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers
 });
 
