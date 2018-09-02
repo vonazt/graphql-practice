bea56d6aee6f0005f71e501a8e8ed7c9021d21d9
Richard Tzanov <richardtzanov@gmail.com>
2018-09-02 12:28:21 +0100

    added links type and resolvers

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,14 +1,33 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+const links = [{
+  id: 'link-0',
+  url: 'www.how-to-graphql.com',
+  description: 'fallback tutorial for GraphQL'
+}];
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
 const resolvers = {
   Query: {
-    info: () => 'This is the API of a Hackernews Clone'
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links
+  },
+  Link: {
+    id: (root) => root.id,
+    description: (root) => root.description,
+    url: (root) => root.url
   }
 };
 
