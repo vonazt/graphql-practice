b3da54b5ea1cec3eff2edf9ab4e0bfeeb88b146b
Richard Tzanov <richardtzanov@gmail.com>
2018-09-02 14:13:40 +0100

    connected server and database with prisma bindings

--- hackernews-node/src/index.js
+++ hackernews-node/src/index.js
@@ -1,34 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const links = [{
-  id: 'link-0',
-  url: 'www.how-to-graphql.com',
-  description: 'fallback tutorial for GraphQL'
-}];
-
-let idCount = links.length;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
     info: () => 'This is the API of a Hackernews Clone',
-    feed: () => links
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info);
+    }
   },
   Mutation: {
-    post: (root, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      }, info);
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: './src/generated/prisma.graphql',
+      endpoint: 'https://eu1.prisma.sh/richardtzanov-38ba04/test-graphql/dev',
+      secret: 'mysecret123',
+      debug: true
+    })
+  })
 });
 
 server.start(() => console.log('Server is running on http://localhost:4000'));
